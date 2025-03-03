2c14ea91636a66490932aea2f1d748c753ee3250
danyesc <144982256+danyesc@users.noreply.github.com>
2025-03-03 11:55:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,38 +11,31 @@ const upload = require('./routes/posts').upload; // Importa la configuración de
 
 const app = express();
 const postsRoutes = require('./routes/posts');
+const UserRoutes = require('./routes/users');
+
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error al conectar con MongoDB:', err));
 
 // Configura CORS para permitir solicitudes desde tu frontend
 const corsOptions = {
-  origin: 'https://lydytravel.site',  // Permitir solo solicitudes desde este dominio
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Permitir estos métodos HTTP
-  allowedHeaders: ['Content-Type', 'Authorization'],  // Permitir estos encabezados
-};
-
-app.use(cors(corsOptions));  // Habilitar CORS con la configuración
-app.options('*', cors(corsOptions));  // Responder a solicitudes OPTIONS (preflight)
+    origin: 'https://lydytravel.site',  // Permitir solo solicitudes desde este dominio
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Permitir estos métodos HTTP
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Permitir estos encabezados
+  };
+  
+  app.use(cors(corsOptions));  // Habilitar CORS con la configuración
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Conexión a MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('Error al conectar con MongoDB:', err));
-
 // Las rutas *van primero*
-app.use('/api/users', require('./routes/users'));
+app.use('/api/users', UserRoutes);
 app.use('/api/posts', postsRoutes);
 
-// Sirve los archivos estáticos de tu frontend
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/uploads', express.static('uploads'));
 
-// Si el frontend está en producción, maneja la ruta principal
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // Puerto de escucha
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
